1befa2f2e62d6a6c12aaa3b63d8aa6677b173d49
BcTorq <briandprivate@hotmail.com>
2024-01-23 13:12:58 -0500

    centralized cache busting

---
+++ version.js
@@ -0,0 +1,5 @@
+// I'm doing this for centralized cache busting
+// Cache busting is being used for both CSS and JavaScript files
+
+// Change the version number here and it changes it for the CSS and JavaScript files version 
+var version = '2.0';
