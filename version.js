cb1bdabb5f7ff45213132bfdc04414fbde22a2c8
BcTorq <briandprivate@hotmail.com>
2024-01-27 18:36:41 -0500

    version control

--- version.js
+++ version.js
@@ -2,4 +2,4 @@
 // Cache busting is being used for both CSS and JavaScript files
 
 // Change the version number here and it changes it for the CSS and JavaScript files version 
-var version = '2.1';
+var version = '3.0';
